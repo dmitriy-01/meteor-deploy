b8eaa4b718979fb634393049ab4027387fe1835a
Dmitriy <dmitriy.refineddata@gmail.com>
2014-06-11 17:55:58 -0400

    updates

--- meteor-deploy.sh
+++ meteor-deploy.sh
@@ -112,6 +112,7 @@ function run() {
 
     echo "Starting forever";
     forever restart bundle/main.js || forever start bundle/main.js;
+    forever logs
 
     echo "Application is running on $ROOT_URL"
 }
